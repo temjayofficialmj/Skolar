eee8e9f02e87267b6090a642f9aa6258f882c05e
TEMJAY <temjayofficialmj@gmail.com>
2025-10-08 12:42:29 +0200

    reconfigured settings for cloud storage

--- Scholar/settings.py
+++ Scholar/settings.py
@@ -150,8 +150,8 @@ if USE_CLOUDINARY:
         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
     }
-    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-    MEDIA_URL = '/media/'
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    MEDIA_URL = 'https://res.cloudinary.com/' + os.environ.get('CLOUDINARY_CLOUD_NAME') + '/media/'
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
